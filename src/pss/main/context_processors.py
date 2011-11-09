cebffd7fc0f0a0e62f2acb1db44deb00ac871543
Samad Jawaid <jawaidss@rose-hulman.edu>
2011-11-08 23:18:58 -0500

    Fix Bug #27

--- src/pss/main/context_processors.py
+++ src/pss/main/context_processors.py
@@ -1,12 +1,20 @@
 from datetime import date
 
 from django.conf import settings
+from django.core.urlresolvers import reverse
 
 def pss_context_processor(request):
+    active = {
+        'home': request.path == reverse('main-index'),
+        'experiments': request.path.startswith('/experiments'), # Hacky...
+        'log_in': request.path == reverse('auth_login'),
+        'sign_up': request.path == reverse('registration_register'),
+    }
     year = date.today().year
     if year > 2011:
         year = u'2011-%s' % year
     return {
+        'active': active,
         'SITE_DOMAIN': settings.SITE_DOMAIN,
         'SITE_NAME': settings.SITE_NAME,
         'YEAR': year,
